c96f855e8eefce8c95dc53a0eb9e08077b93c30e
ariya-1210 <ariyailiyahad@gmail.com>
2023-07-28 07:30:50 +0500

    navbar

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Button from '../Generic/Button'
 import { Container } from './style'
 
 export const Home = () => {
   return (
     <Container>
        Home Houzing
+       <Button type={'light'}>Salom</Button>
     </Container>
   )
 }
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Container, Logo, NavItem, Section, Wrapper,Main } from './style'
+import {navbar} from '../../utils/navbar'
+import { Outlet, useNavigate } from 'react-router-dom'
+import Button from '../Generic/Button'
+
+export const Navbar = () => {
+  const navigate=useNavigate()
+  return (
+    <Container>
+      <Main>
+      
+      <Wrapper>
+        <Section logo onClick={()=>navigate('/home') }><Logo/> <h3>Houzing</h3></Section>
+        <Section>
+          {
+            navbar.map(({path,title,hidden},index)=>{
+              return !hidden &&(
+               <NavItem className={({isActive})=>isActive && 'active'}          
+                key={index} to={path}>{title}</NavItem>
+              )
+            })
+          }
+          
+      
+        </Section>
+        <Section><Button onClick={()=>navigate('/signin')} type={'dark'}>Sing In</Button></Section>
+      </Wrapper>
+        
+      </Main>
+      <Outlet/>
+    </Container>
+  )
+}
+export default Navbar
