582a019e5e809fc7957a495cb5796c78ea241a4f
ariya-1210 <ariyailiyahad@gmail.com>
2023-08-23 08:02:52 +0500

    navbar

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Container } from './style'
 import CarouselCard from '../Carousel'
-import HouseCard from '../HouseCard'
 
 const Home = () => {
   return (
     <Container>
-      <CarouselCard/>  
-      <HouseCard/>
+      <CarouselCard/>
     </Container>
   )
 }
--- src/components/Properties/index.jsx
+++ src/components/Properties/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Container } from './style'
+import HouseCard from '../HouseCard'
 
 const Properties = () => {
   return (
     <Container>
-        Properties
+        <HouseCard/>
     </Container>
   )
 }
